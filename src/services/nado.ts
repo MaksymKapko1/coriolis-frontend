8ee9cb170d3ee7275f672a9ec263360fbe2d21ef
maksym <kapkomaxim@gmail.com>
2026-05-26 09:08:11 +0300

    add nado client to api

---
+++ src/services/nado.ts
@@ -0,0 +1,23 @@
+import { createNadoClient } from '@nadohq/client';
+import { createPublicClient, createWalletClient, custom, http } from 'viem';
+import { inkSepolia } from 'viem/chains';
+
+export const getPrivyNadoClient = async (privyProvider: any, walletAddress: `0x${string}`) => {
+  const walletClient = createWalletClient({
+    account: walletAddress,
+    chain: inkSepolia,
+    transport: custom(privyProvider),
+  });
+
+  const publicClient = createPublicClient({
+    chain: inkSepolia,
+    transport: http(),
+  });
+
+  const nadoClient = await createNadoClient('inkTestnet', {
+    walletClient,
+    publicClient,
+  });
+
+  return nadoClient;
+};
